eb45c344ee9ee1fe5ef8d5057b13114e4b653253
MaxTW <534983@student.saxion.nl>
2025-06-05 14:37:31 +0200

    Login re design

--- app/LoginScreen.tsx
+++ app/LoginScreen.tsx
@@ -1,25 +1,39 @@
 import LoginInput from "../components/auth/LoginInput";
-import { Image, StyleSheet, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from 'react';
 
 export default function LoginScreen() {
     return (
         <View style={screenStyle.container}>
-            <Image style={screenStyle.image} source = {require('../assets/images/Filametric_Full_Logo_v3.png')} />
-            <LoginInput></LoginInput>
+            <View style={screenStyle.header}>
+                <Image style={screenStyle.image} source={require('../assets/images/Filametric_F_logo.png')} />
+                <Text style={screenStyle.titleText}>Log in or sign up</Text>
+            </View>
+            <LoginInput />
         </View>
     )
 }
 
 const screenStyle = StyleSheet.create({
+    header: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 32,
+        marginTop: 32
+    },
+    titleText: {
+        fontFamily: "Satoshi",
+        fontSize: 32,
+        fontWeight: "700",
+        marginTop: 24,
+    },
     container: {
         margin: 20,
         flex: 1,
-        justifyContent: 'center',
     },
     image: {
-        width: "100%",
-        height: 100,
+        width: 57,
+        height: 54,
         resizeMode: 'contain',
     },
-})
+});
--- components/auth/LoginInput.tsx
+++ components/auth/LoginInput.tsx
@@ -1,14 +1,18 @@
 import { AuthContext } from "@/context/authContext";
 import { router } from "expo-router";
 import { useContext, useState } from "react";
-import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import { Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { Auth } from "aws-amplify";
+import { Ionicons } from "@expo/vector-icons";
 
 const LoginInput = () => {
     const { setUser } = useContext(AuthContext);
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+
+    const [secureText, setSecureText] = useState(true);
+    const [checked, setChecked] = useState(false);
 
     const [loginError, setLoginError] = useState<string>("");
 
@@ -57,56 +61,188 @@ const LoginInput = () => {
                 value={email}
                 onChangeText={setEmail}
                 placeholderTextColor="gray"
-                placeholder="Email"
+                placeholder="Email Address"
                 style={style.textField}
                 autoCapitalize="none"
                 keyboardType="email-address"
             />
-            <TextInput
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry={true}
-                placeholderTextColor="gray"
-                placeholder="Password"
-                style={style.textField}
-            />
-            {loginError.length > 0 && (
-                <Text style={style.errorText}>{loginError}</Text>
-            )}
+            
+            <View style={style.passwordContainer}>
+                <TextInput
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry={secureText}
+                    placeholderTextColor="gray"
+                    placeholder="Password"
+                    style={{flex: 1}}
+                />
+                <TouchableOpacity onPress={() => setSecureText(!secureText)} style={style.toggle}>
+                    {secureText ? <Ionicons size={24} name='eye-sharp'/>: <Ionicons size={24} name='eye-off-sharp'/>}
+                </TouchableOpacity>
+            </View>
+
+            <View style={style.optionsRow}>
+                <Pressable style={style.checkboxContainer} onPress={() => setChecked(!checked)}>
+                    <View style={[style.checkbox, checked && style.checked]} />
+                    <Text style={style.label}>Remember me</Text>
+                </Pressable>
+                <Pressable onPress={() => router.push("/")}>
+                    <Text style={style.forgotPasswordText}>Forgot Password?</Text>
+                </Pressable>
+            </View>
+
+            {loginError ? <Text style={style.errorText}>{loginError}</Text> : null}
 
             <Pressable onPress={Login} style={style.button}>
                 <Text style={style.buttonText}>Login</Text>
             </Pressable>
-            <Pressable onPress={GoToRegisterScreen} style={style.button}>
-                <Text style={style.buttonText}>Register</Text>
-            </Pressable>
+
+            <View style={style.dividerContainer}>
+                <View style={style.line} />
+                <Text style={style.dividerText}>or</Text>
+                <View style={style.line} />
+            </View>
+
+            <View style={style.signUpRow}>
+                <Text style={style.signUpQuestionText}>No Filametric account yet?</Text>
+                <Pressable onPress={GoToRegisterScreen} >
+                    <Text style={style.signUpText} >Sign up</Text>
+                </Pressable>
+            </View>
+            
         </View>
     );
 };
 
 const style = StyleSheet.create({
     button: {
-        backgroundColor: "gray",
+        height: 64,
+        backgroundColor: "#FF5500",
+        justifyContent: 'center',
         alignItems: "center",
-        padding: 8,
-        marginTop: 4,
+        marginTop: 8,
         marginBottom: 4,
         borderWidth: 1,
-        borderRadius: 6,
+        borderRadius: 16,
+        borderColor: "#FF5500",
+        paddingTop: 8,
+        paddingRight: 16,
+        paddingBottom: 8,
+        paddingLeft: 16,
     },
     buttonText: {
         color: "white",
+        fontSize: 16,
+        fontFamily: "Satoshi",
     },
     textField: {
         marginTop: 4,
         marginBottom: 4,
-        padding: 8,
+        height: 64,
+        paddingTop: 8,
+        paddingRight: 16,
+        paddingBottom: 8,
+        paddingLeft: 16,
+        borderRadius: 16,
         borderWidth: 1,
-        borderRadius: 6,
+        borderColor: "#E7E7E7",
+        fontFamily: "Satoshi",
+        fontSize: 16,
+        backgroundColor: "white",
+    },
+    passwordContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 4,
+        marginBottom: 4,
+        height: 64,
+        paddingTop: 8,
+        paddingRight: 16,
+        paddingBottom: 8,
+        paddingLeft: 16,
+        borderRadius: 16,
+        borderWidth: 1,
+        borderColor: "#E7E7E7",
+        fontFamily: "Satoshi",
+        fontSize: 16,
+        backgroundColor: "white",
+    },
+    toggle: {
+        paddingHorizontal: 10,
+    },
+    checkboxContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginVertical: 8,
+    },
+    checkbox: {
+        width: 24,
+        height: 24,
+        borderWidth: 2,
+        borderColor: '#000000',
+        marginRight: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 4,
+    },
+    checked: {
+        backgroundColor: '#000000',
+    },
+    label: {
+        fontSize: 16,
+        fontFamily: "Satoshi",
     },
     errorText: {
-        color: "red",
+        color: 'red',
+        marginBottom: 8,
+        fontFamily: "Satoshi",
     },
+    optionsRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginVertical: 16,
+    },
+    forgotPasswordText: {
+        color: '#000000',
+        fontSize: 16,
+        fontFamily: 'Satoshi',
+        textDecorationLine: 'underline',
+    },
+     dividerContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginVertical: 16,
+    },
+    line: {
+        flex: 1,
+        height: 1,
+        backgroundColor: '#aaa',
+    },
+    dividerText: {
+        marginHorizontal: 8,
+        color: '#555',
+        fontSize: 16,
+        fontFamily: 'Satoshi',
+    },
+    signUpRow: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 16,
+    },
+    signUpQuestionText: {
+        marginRight: 16,
+        fontSize: 16,
+        fontFamily: 'Satoshi',
+        fontWeight: "400"
+    },
+    signUpText: {
+        fontSize: 16,
+        fontFamily: 'Satoshi',
+        color: "#FF5500",
+        fontWeight: "700"
+    }
 });
 
 export default LoginInput;
