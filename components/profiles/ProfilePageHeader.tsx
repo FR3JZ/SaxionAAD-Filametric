8b2268b47a2d3ef6cf750669c957f617f594c0ca
Thijs Cameron <514309@student.saxion.nl>
2025-06-05 13:26:55 +0200

    Fetch

--- components/profiles/ProfilePageHeader.tsx
+++ components/profiles/ProfilePageHeader.tsx
@@ -3,6 +3,34 @@ import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 const DryerAddHeader = () => {
+
+    const handlePostRequest = async () => {
+        try {
+            const response = await fetch('https://hkp9clnxq6.execute-api.eu-north-1.amazonaws.com/dev/profiles/add', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                "name": "PETG Quick Dry",
+                "description": "Snel droogprofiel voor PETG filament",
+                "target_temperature": 65.0,
+                "duration": 7200,
+                "drying_mode": "silent",
+                "switch_to_storage": true,
+                "storage_temperature": 30.0
+              }),
+            });
+      
+            const data = await response.json();
+            console.log('Server response:', data);
+            Alert.alert('Succes', 'POST request verstuurd!');
+          } catch (error) {
+            console.error('Fout bij POST request:', error);
+            Alert.alert('Fout', 'Er ging iets mis bij het versturen.');
+          }
+    }
+
     return (
         <View style={styles.headerContainer}>
             <View style={styles.topBar}>
